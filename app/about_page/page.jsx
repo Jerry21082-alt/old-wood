39ddb4a93025bc5755ad807ea176da3aceb0b02f
Jerry21082-alt <123124690+Jerry21082-alt@users.noreply.github.com>
2024-08-02 03:03:26 +0100

    initial commit

--- app/about_page/page.jsx
+++ app/about_page/page.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 export default function page() {
@@ -121,6 +122,89 @@ export default function page() {
                 </div>
               </div>
             </div>
+          </div>
+        </div>
+      </div>
+      <div className="my-7">
+        <div>
+          <div className="-mb-7 pt-7 pb-7">
+            <div className="w-full max-w-[600px] px-6">
+              <div className="flex flex-wrap relative overflow-hidden">
+                <div className="mt-[-50%] pl-[30%] order-2 w-full relative">
+                  <img
+                    src="//roweam.com/cdn/shop/files/101032_Art_Deco_Danish_Armchair_1930s_0536_Resized.jpg?v=1694800087&width=1400"
+                    srcSet="//roweam.com/cdn/shop/files/101032_Art_Deco_Danish_Armchair_1930s_0536_Resized.jpg?v=1694800087&width=600 600w, //roweam.com/cdn/shop/files/101032_Art_Deco_Danish_Armchair_1930s_0536_Resized.jpg?v=1694800087&width=700 700w, //roweam.com/cdn/shop/files/101032_Art_Deco_Danish_Armchair_1930s_0536_Resized.jpg?v=1694800087&width=800 800w, //roweam.com/cdn/shop/files/101032_Art_Deco_Danish_Armchair_1930s_0536_Resized.jpg?v=1694800087&width=1000 1000w, //roweam.com/cdn/shop/files/101032_Art_Deco_Danish_Armchair_1930s_0536_Resized.jpg?v=1694800087&width=1200 1200w, //roweam.com/cdn/shop/files/101032_Art_Deco_Danish_Armchair_1930s_0536_Resized.jpg?v=1694800087&width=1400 1400w"
+                    alt="product image"
+                    className="w-full max-w-full"
+                  />
+
+                  <div className="py-[15px]">
+                    <h5 className="text-[14px] normal-case">
+                      Adorning life with handmade details.
+                    </h5>
+                  </div>
+                </div>
+                <div className="w-full z-[1]">
+                  <div className="flex flex-nowrap">
+                    <div className="shrink-0 w-full">
+                      <div className="my-[40px] mr-4">
+                        <div style={{ opacity: 1 }} className="h4">
+                          <span className="block">
+                            Furniture is a gathering place
+                          </span>
+                          <span className="block">
+                            And our curated collection is
+                          </span>
+                          <span className="block">the conversation piece.</span>
+                        </div>
+                      </div>
+                      <div style={{ opacity: "1" }}>
+                        <Link
+                          href="/"
+                          className="text-lightBrown whitespace-nowrap w-max uppercase inline-block relative text-sm shop-button"
+                        >
+                          Shop now
+                        </Link>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="my-7 px-6">
+        <div className="w-full">
+          <div className="flex flex-wrap w-full space-x-7">
+            <div className="pb-[30px] pt-[60px] pr-[60px] z-[1]">
+              <h5 className="my-[15px] text-lightBrown text-[14px]">Process</h5>
+              <div
+                className="mt-4 mr-4 h2 text-[46px]"
+                style={{ lineHeight: 1 }}
+              >
+                The Oldood Method
+              </div>
+              <div className="max-w-[90%] mt-6">
+                <p>
+                  Part new vintage, part true vintage, our design method takes a
+                  gathered, authentic approach using well-constructed pieces
+                  complete with endless character, no matter their age. At
+                  Roweam, we believe character is a cornerstone of timeless
+                  interiors. We create pieces that are made to age and live
+                  through generations.{" "}
+                </p>
+                <div className="mt-8">
+                  <Link
+                    href="/"
+                    className="shop-button relative uppercase text-sm text-lightBrown"
+                  >
+                    Shop now
+                  </Link>
+                </div>
+              </div>
+            </div>
+            <div></div>
           </div>
         </div>
       </div>
